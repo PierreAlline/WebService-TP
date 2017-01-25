8a96a55562b529207d8b3a75e86e98bd191efb0f
Pierre Alline <pierrealline@hotmail.fr>
2017-01-25 15:27:00 +0100

    Remove

--- src/hello/HelloWorldLanceur.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-import javax.xml.ws.Endpoint;
-
-public class HelloWorldLanceur {
-    public static void main(String[] args) {
-        Endpoint.publish("http://localhost:9999/ws/bonjour", new HelloWorldImpl());
-    }
-}
