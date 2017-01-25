649af9b00fec99bb26bd5fd5158638b4078059ae
Pierre Alline <pierrealline@hotmail.fr>
2017-01-25 15:34:54 +0100

    Restructuration_Final

---
+++ WebService-1/src/hello/HelloWorldLanceur.java
@@ -0,0 +1,9 @@
+package hello;
+
+import javax.xml.ws.Endpoint;
+
+public class HelloWorldLanceur {
+    public static void main(String[] args) {
+        Endpoint.publish("http://localhost:9999/ws/bonjour", new HelloWorldImpl());
+    }
+}
